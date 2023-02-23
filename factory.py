d65baf8d3cb20d51272cb6f4ee806295cf71c3a5
pravar <pravar.sharma@trootech.com>
2023-02-23 18:11:59 +0530

    removed pycache

--- factory.py
+++ factory.py
@@ -1,19 +1,20 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-from api.rest import users
-
 from conf.database import db, bcrypt, Config
 
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_object(Config)
-    app.register_blueprint(users.USER_BLUEPRINT)
     db.init_app(app)
     bcrypt.init_app(app)
     Migrate(app, db)
+
+    from api.rest import users
+    app.register_blueprint(users.USER_BLUEPRINT)
     with app.app_context():
         db.create_all()
     return app
